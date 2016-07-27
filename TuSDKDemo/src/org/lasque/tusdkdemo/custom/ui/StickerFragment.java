3c4f40675b7194b00f86c8827ea489d7848804aa
Amberlo <297059524@qq.com>
2016-07-27 20:48:39 +0800

    commit

--- TuSDKDemo/src/org/lasque/tusdkdemo/custom/ui/StickerFragment.java
+++ TuSDKDemo/src/org/lasque/tusdkdemo/custom/ui/StickerFragment.java
@@ -1,52 +1,21 @@
-/** 
+/**
  * TuSdkDemo
  * DemoEntryActivity.java
  *
  * @author 		Clear
- * @Date 		2014-11-15 下午4:30:52 
+ * @Date 		2014-11-15 下午4:30:52
  * @Copyright 	(c) 2014 tusdk.com. All rights reserved.
  * @link 		开发文档:http://tusdk.com/docs/android/api/
  */
 package org.lasque.tusdkdemo.custom.ui;
 
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-
-import com.example.abner.stickerdemo.utils.FileUtils;
-import com.example.abner.stickerdemo.view.BubbleInputDialog;
-import com.example.abner.stickerdemo.view.BubbleTextView;
-import com.flask.colorpicker.ColorPickerView;
-import com.flask.colorpicker.OnColorSelectedListener;
-import com.flask.colorpicker.builder.ColorPickerClickListener;
-import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
-
-import org.lasque.tusdk.core.TuSdkContext;
-import org.lasque.tusdk.core.TuSdkResult;
-import org.lasque.tusdk.core.secret.SdkValid;
 import org.lasque.tusdk.core.secret.StatisticsManger;
-import org.lasque.tusdk.core.view.widget.button.TuSdkImageButton;
-import org.lasque.tusdk.core.view.widget.button.TuSdkTextButton;
-import org.lasque.tusdk.impl.activity.TuImageResultFragment;
 import org.lasque.tusdk.impl.components.sticker.TuEditStickerFragment;
 import org.lasque.tusdk.impl.components.widget.sticker.StickerBarView;
 import org.lasque.tusdk.modules.components.ComponentActType;
-import org.lasque.tusdk.modules.view.widget.sticker.StickerCategory;
-import org.lasque.tusdk.modules.view.widget.sticker.StickerLocalPackage;
 import org.lasque.tusdkdemo.R;
-import org.lasque.tusdkdemo.custom.CustomStickerBarView;
-import org.lasque.tusdkdemo.custom.suite.TextStickerComponentOption;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Amberllo
@@ -60,9 +29,8 @@ public class StickerFragment extends TuEditStickerFragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
-        this.setRootViewLayoutId(getLayoutViewId());
-        return !SdkValid.shared.sdkValid()?null:super.onCreateView(layoutInflater, viewGroup, bundle);
+    public void setRootViewLayoutId(int i) {
+        super.setRootViewLayoutId(getLayoutViewId());
     }
 
     @Override
@@ -80,19 +48,24 @@ public class StickerFragment extends TuEditStickerFragment
         }
 
 
-//        CustomStickerBarView stickerBarView = (CustomStickerBarView)viewGroup.findViewById(R.id.lsq_sticker_bar);
-//        if(stickerBarView != null) {
-//            stickerBarView.setGridLayoutId(this.getGridLayoutId());
-//            stickerBarView.setGridWidth(this.getGridWidth());
-//            stickerBarView.setGridPadding(this.getGridPadding());
-//            stickerBarView.setGridHeight(this.getGridHeight());
-//            stickerBarView.setDelegate(this);
-//            stickerBarView.loadCategories(this.getCategories());
-//        }
-
     }
 
 
+    @Override
+    public StickerBarView getStickerBarView() {
 
 
+        StickerBarView k = (StickerBarView)this.getViewById("lsq_sticker_bar");
+            if( k != null) {
+                k.setGridLayoutId(this.getGridLayoutId());
+                k.setGridWidth(this.getGridWidth());
+                k.setGridPadding(this.getGridPadding());
+                k.setGridHeight(this.getGridHeight());
+                k.setDelegate(this);
+            }
+
+        return k;
+
+
+    }
 }
