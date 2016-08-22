0bfd95b1bc406e1a02cf9af254f29e5c3cb1cba5
Amberlo <297059524@qq.com>
2016-08-23 01:55:00 +0800

    编辑照片界面修改

--- app/src/com/share/photoshare/custom/ui/SkinFragment.java
+++ app/src/com/share/photoshare/custom/ui/SkinFragment.java
@@ -2,24 +2,12 @@ package com.share.photoshare.custom.ui;
 
 import com.share.photoshare.R;
 
-import org.lasque.tusdk.core.seles.tusdk.FilterWrap;
-
-import org.lasque.tusdk.impl.activity.TuFilterResultFragment;
 import org.lasque.tusdk.impl.components.filter.TuEditSkinFragment;
 
 /**
  * Created by apple on 16/8/22.
  */
 public class SkinFragment extends TuEditSkinFragment{
-
-    private FilterWrap filterWrap;
-    TuFilterResultFragment.TuFilterResultFragmentDelegate delegate;
-
-
-    public void setDelegate(TuFilterResultFragment.TuFilterResultFragmentDelegate delegate) {
-        super.setDelegate(delegate);
-        this.delegate = delegate;
-    }
 
     public static int getLayoutViewId(){
         return R.layout.custom_skin_fragment_layout;
@@ -32,12 +20,6 @@ public class SkinFragment extends TuEditSkinFragment{
 
 
     @Override
-    protected void setImageViewFilter(FilterWrap filterWrap) {
-        super.setImageViewFilter(filterWrap);
-        this.filterWrap = filterWrap;
-    }
-
-    @Override
     public TuFilterResultFragmentDelegate getDelegate() {
         TuFilterResultFragmentDelegate  delegate = super.getDelegate();
         return delegate;
@@ -46,19 +28,6 @@ public class SkinFragment extends TuEditSkinFragment{
     @Override
     protected void handleCompleteButton() {
         super.handleCompleteButton();
-//        final TuSdkResult tuSdkResult;
-//        (tuSdkResult = new TuSdkResult()).filterWrap = this.filterWrap;
-//        loadOrginImage(tuSdkResult);
-//        if(tuSdkResult.filterWrap != null) {
-//            float var2 = TuSdkSize.create(tuSdkResult.image).limitScale();
-//            tuSdkResult.image = BitmapHelper.imageScale(tuSdkResult.image, var2);
-//            tuSdkResult.image = tuSdkResult.filterWrap.clone().process(tuSdkResult.image);
-//            tuSdkResult.imageFile = new File(FileUtils.saveBitmapToLocal(tuSdkResult.image,getContext()));
-//        }
-//
-//        if(delegate!=null){
-//            delegate.onTuFilterResultFragmentEdited(SkinFragment.this,tuSdkResult);
-//        }
         handleBackButton();
     }
 
